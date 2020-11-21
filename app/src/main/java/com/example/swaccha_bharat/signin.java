3f05b20d0a8722170593823e46b8223210b92b62
Aviral00 <aviralsrivastava1404@gmail.com>
2020-11-21 12:54:55 +0530

    First Commit

--- app/src/main/java/com/example/swaccha_bharat/signin.java
+++ app/src/main/java/com/example/swaccha_bharat/signin.java
@@ -2,16 +2,13 @@ package com.example.swaccha_bharat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class signin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signin2);
     }
 }
